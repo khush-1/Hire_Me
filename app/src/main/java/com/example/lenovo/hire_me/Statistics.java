0b540c0ab75280c7c064beebbc80abd76f5da89e
khush-1 <khushbooagarwal947@gmail.com>
2019-01-26 04:42:55 +0530

    cardview added

--- app/src/main/java/com/example/lenovo/hire_me/Statistics.java
+++ app/src/main/java/com/example/lenovo/hire_me/Statistics.java
@@ -3,11 +3,44 @@ package com.example.lenovo.hire_me;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class Statistics extends AppCompatActivity {
+    FirebaseAuth mAuth;
+    FirebaseDatabase database;
+    DatabaseReference databaseReference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
+        mAuth=FirebaseAuth.getInstance();
+        database=FirebaseDatabase.getInstance();
+        databaseReference=database.getReference().child("curstatistics");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot iterator:dataSnapshot.getChildren()){
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
