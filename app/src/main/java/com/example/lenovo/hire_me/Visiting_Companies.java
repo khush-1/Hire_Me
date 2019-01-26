48a059ab03c0242dfe180bfcb4e2a669d715ee49
saraswati789 <vandana.saras11@gmail.com>
2019-01-26 08:51:43 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser
    #	app/src/main/AndroidManifest.xml

---
+++ app/src/main/java/com/example/lenovo/hire_me/Visiting_Companies.java
@@ -0,0 +1,13 @@
+package com.example.lenovo.hire_me;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Visiting_Companies extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visiting__companies);
+    }
+}
