50c5d0c504ba5f03c688934daead083db634e6ef
saraswati789 <vandana.saras11@gmail.com>
2019-01-27 05:22:17 +0530

    miscellaneous

--- app/src/main/java/com/example/lenovo/hire_me/ApplyNow.java
+++ app/src/main/java/com/example/lenovo/hire_me/ApplyNow.java
@@ -1,15 +1,33 @@
 package com.example.lenovo.hire_me;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class ApplyNow extends AppCompatActivity {
+public class ApplyNow extends DialogFragment {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_apply_now);
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(R.string.dialog_fire_missiles)
+                .setPositiveButton(R.string.fire, new DialogInterface.OnClickListener() {
 
-
+                    public void onClick(DialogInterface dialog, int id) {
+                        // FIRE ZE MISSILES!
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
     }
+
+
 }
